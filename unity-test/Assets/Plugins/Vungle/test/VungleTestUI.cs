5f7dd0e076f002cbc03b034626cd5534485f374d
AndreyK <kosyakow@gmail.com>
2015-09-11 17:01:16 +0300

    Remove fix with reinit

--- unity-test/Assets/Plugins/Vungle/test/VungleTestUI.cs
+++ unity-test/Assets/Plugins/Vungle/test/VungleTestUI.cs
@@ -181,8 +181,7 @@ public class VungleTestUI : MonoBehaviour
 			userTag = GUILayout.TextField (userTag);
 
 			if (GUILayout.Button ("Init")) {
-				if (!bInit)
-					Vungle.init (appId, appId);
+				Vungle.init (appId, appId);
 				bInit = true;
 				if (Vungle.isAdvertAvailable ())
 					adAvailable = true;
