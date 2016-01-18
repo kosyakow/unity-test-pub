25b3144e34393e341c5728945413dd251beb47b7
Andrey Kosyakov <andreyk@local.pushok.com>
2016-01-18 19:11:39 +0300

    PLUG-357 Lots of warning shows up in console of Unity

--- unity-test/Assets/Plugins/Vungle/test/VungleTestUI.cs
+++ unity-test/Assets/Plugins/Vungle/test/VungleTestUI.cs
@@ -115,21 +115,20 @@ public class VungleTestUI : MonoBehaviour
 	}
 
 	Dictionary<string,object> formatOptions() {
-		var orientation = 0;
+		Dictionary<string,object> options = new Dictionary<string,object> ();
 		#if UNITY_IPHONE
-		orientation = 5;
+		var orientation = 5;
 		if (landscape && !portrait) {
 			orientation = 4;
 		}
 		else if (portrait)
 			orientation = 3;
+		options.Add ("orientation", orientation);
 		#endif
 		#if UNITY_ANDROID || UNITY_WSA_10_0 || UNITY_WINRT_8_1 || UNITY_METRO
-		orientation = landscape?true:false;
+		options.Add ("orientation", landscape?true:false);
 		#endif
-		Dictionary<string,object> options = new Dictionary<string,object> ();
 		options.Add ("incentivized", incentivized);
-		options.Add ("orientation", orientation);
 		options.Add ("large", large);
 		if (userTag != "")
 			options.Add ("userTag", userTag);
@@ -268,10 +267,8 @@ public class VungleTestUI : MonoBehaviour
 	void OnEnable()
 	{
 		Vungle.onAdStartedEvent += onAdStartedEvent;
-		Vungle.onAdEndedEvent += onAdEndedEvent;
-		Vungle.onAdViewedEvent += onAdViewedEvent;
 		Vungle.adPlayableEvent += adPlayableEvent;
-		Vungle.onCachedAdAvailableEvent += onCachedAdAvailableEvent;
+		Vungle.onAdFinishedEvent += onAdFinishedEvent;
 		Vungle.onLogEvent += onLogEvent;
 	}
 
@@ -279,10 +276,8 @@ public class VungleTestUI : MonoBehaviour
 	void OnDisable()
 	{
 		Vungle.onAdStartedEvent -= onAdStartedEvent;
-		Vungle.onAdEndedEvent -= onAdEndedEvent;
-		Vungle.onAdViewedEvent -= onAdViewedEvent;
 		Vungle.adPlayableEvent -= adPlayableEvent;
-		Vungle.onCachedAdAvailableEvent -= onCachedAdAvailableEvent;
+		Vungle.onAdFinishedEvent -= onAdFinishedEvent;
 		Vungle.onLogEvent -= onLogEvent;
 	}
 	
@@ -301,20 +296,6 @@ public class VungleTestUI : MonoBehaviour
 	}
 
 
-	void onAdEndedEvent()
-	{
-		log = "onAdEndedEvent\n" + log;
-		Debug.Log( "onAdEndedEvent" );
-	}
-
-
-	void onAdViewedEvent( double watched, double length )
-	{
-		log = "onAdViewedEvent: " + watched + ", " + length + "\n" + log;
-		Debug.Log( "onAdViewedEvent. watched: " + watched + ", length: " + length );
-	}
-
-
 	void adPlayableEvent(bool available)
 	{
 		log = "adPlayableEvent: " + available + "\n" + log;
@@ -322,12 +303,12 @@ public class VungleTestUI : MonoBehaviour
 		Debug.Log( "onCachedAdAvailableEvent" );
 	}
 	
-	void onCachedAdAvailableEvent()
+	void onAdFinishedEvent(AdFinishedEventArgs arg)
 	{
-		log = "onCachedAdAvailableEvent\n" + log;
-		Debug.Log( "onCachedAdAvailableEvent" );
+		log = "onAdFinishedEvent. watched: " + arg.TimeWatched + ", length: " + arg.TotalDuration  + ", isCompletedView: " + arg.IsCompletedView + "\n" + log;
+		Debug.Log( "onAdFinishedEvent" );
 	}
-	
+
 	void onLogEvent(string logMessage)
 	{
 		log = logMessage + "\n" + log;
