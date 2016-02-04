79cc027e5a6e9649afe9ac86633be110678d47ac
Alexander Somov <alexs@pushok.com>
2016-02-04 03:14:24 -0800

    Fix zip

--- uwpBuild.sh
+++ uwpBuild.sh
@@ -23,10 +23,10 @@ echo "Import plugin..."
 "c:/Program Files/Unity/Editor/Unity" -quit -batchmode -nographics -projectPath $PROJECT_PATH/unity-test -importPackage $PROJECT_PATH/VungleSDK.unitypackage
 
 echo "Build unity project..."
-"c:/Program Files/Unity/Editor/Unity" -quit -batchmode -nographics -projectPath $PROJECT_PATH/unity-test -executeMethod BuildGenerator.BuildUWP -CustomArgs:ver=$version.$build
+"c:/Program Files/Unity/Editor/Unity" -quit -batchmode -nographics -projectPath $PROJECT_PATH/unity-test -executeMethod BuildGenerator.BuildUWP -CustomArgs:ver=$version.$build.0
 
 echo "Build unity project again..."
-"c:/Program Files/Unity/Editor/Unity" -quit -batchmode -nographics -projectPath $PROJECT_PATH/unity-test -executeMethod BuildGenerator.BuildUWP -CustomArgs:ver=$version.$build
+"c:/Program Files/Unity/Editor/Unity" -quit -batchmode -nographics -projectPath $PROJECT_PATH/unity-test -executeMethod BuildGenerator.BuildUWP -CustomArgs:ver=$version.$build.0
 
 cp uwp/VungleUnity5Test.csproj.user unity-test/Builds/WSA/VungleUnity5Test_win10/VungleUnity5Test_win10.csproj.user
 cp uwp/UnityCommon.props unity-test/Builds/WSA/UnityCommon.props
